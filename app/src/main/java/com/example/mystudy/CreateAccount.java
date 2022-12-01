028db28042ad9470404902e3c4fbeb0cf4dac43a
Sammich99 <109836180+Sammich99@users.noreply.github.com>
2022-12-01 00:56:44 -0500

    sign up and login screen start activity

--- app/src/main/java/com/example/mystudy/CreateAccount.java
+++ app/src/main/java/com/example/mystudy/CreateAccount.java
@@ -4,12 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CreateAccount extends AppCompatActivity {
 
     Button SignUpButton;
+    TextView LoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +19,20 @@ public class CreateAccount extends AppCompatActivity {
         setContentView(R.layout.create_account);
 
         SignUpButton =  findViewById(R.id.SignUpButton);
+        LoginButton = findViewById(R.id.LoginButton);
 
         SignUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(CreateAccount.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =  new Intent(CreateAccount.this, Login.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/mystudy/Login.java
+++ app/src/main/java/com/example/mystudy/Login.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.mystudy.CreateAccount;
 import com.example.mystudy.R;
@@ -13,6 +14,7 @@ import com.example.mystudy.R;
 public class Login extends AppCompatActivity {
 
     Button loginButton;
+    TextView signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +22,17 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.login);
 
         loginButton =  findViewById(R.id.loginButton);
+        signUpButton = findViewById(R.id.signUpButton);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Login.this, CreateAccount.class);
