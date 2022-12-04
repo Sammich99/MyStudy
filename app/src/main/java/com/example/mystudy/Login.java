ee348722d396626f2904f01109fb0ce16e6ce9e3
Sammich99 <109836180+Sammich99@users.noreply.github.com>
2022-12-03 23:02:53 -0500

    login and sign up implemented with sqlitedatabase

--- app/src/main/java/com/example/mystudy/Login.java
+++ app/src/main/java/com/example/mystudy/Login.java
@@ -6,7 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.mystudy.CreateAccount;
 import com.example.mystudy.R;
@@ -15,6 +17,9 @@ public class Login extends AppCompatActivity {
 
     Button loginButton;
     TextView signUpButton;
+    EditText loginemailaddress, loginpassword;
+    AccountDatabase accountdb;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +28,28 @@ public class Login extends AppCompatActivity {
 
         loginButton =  findViewById(R.id.loginButton);
         signUpButton = findViewById(R.id.signUpButton);
+        loginemailaddress = findViewById(R.id.loginemailaddress);
+        loginpassword = findViewById(R.id.loginpassword);
+        accountdb = new AccountDatabase(this);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Login.this, MainActivity.class);
-                startActivity(intent);
+                String email = loginemailaddress.getText().toString();
+                String pass = loginpassword.getText().toString();
+
+                if(email.equals("")||pass.equals(""))
+                    Toast.makeText(Login.this, "Please enter all fields", Toast.LENGTH_SHORT).show();
+                else{
+                    Boolean checkemailpass = accountdb.checkemailaddresspassword(email, pass);
+                    if(checkemailpass == true){
+                        Toast.makeText(Login.this, "Login Successfull", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent);
+                    } else{
+                        Toast.makeText(Login.this, "Incorrect Details", Toast.LENGTH_SHORT).show();
+                    }
+                }
             }
         });
 
